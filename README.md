97ca8c76481112f60d3c47725068718be9353206
ryanpjhickey <ryanpjhickey@gmail.com>
2023-01-17 17:57:48 -0800

    Upload new Portfolio

--- README.md
+++
@@ -1,3 +0,0 @@
-# This is my React Portfolio!
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/PortfolioHeader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function PortfolioHeader() {
-    return (
-        <div>
-            <h1 class="pheader"><a href="">Ryan Hickey</a></h1>
-        </div >
-    );
-}
